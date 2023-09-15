490b8a0d7261767eb3763ef69620dc76e9612ecf
jpablopachar <jpablopachar1993@gmail.com>
2023-09-15 03:14:54 -0500

    Creación de SpinnerComponent

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,33 +1,19 @@
 import { CommonModule } from '@angular/common'
-import { Component, OnInit, inject } from '@angular/core'
-import {
-  CollectionReference,
-  DocumentData,
-  Firestore,
-  collection,
-  collectionData,
-} from '@angular/fire/firestore'
+import { Component, WritableSignal, signal } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { Observable } from 'rxjs'
+import { SpinnerComponent } from './shared'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, SpinnerComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  private _firestore: Firestore = inject(Firestore);
+export class AppComponent {
+  public showSpinner: WritableSignal<boolean> = signal(false);
 
-  public items$!: Observable<any[]>;
-
-  ngOnInit(): void {
-    const abc: CollectionReference<DocumentData> = collection(
-      this._firestore,
-      'test'
-    );
-
-    this.items$ = collectionData(abc);
+  public onToggleSpinner(): void {
+    this.showSpinner.set(!this.showSpinner());
   }
 }
