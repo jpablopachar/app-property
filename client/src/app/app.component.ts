b88b08a5bdc511b3507a26e930ac86254e37679c
jpablopachar <jpablopachar1993@gmail.com>
2023-09-15 23:21:53 -0500

    Se implementa sidenav

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { CommonModule } from '@angular/common'
 import { Component, WritableSignal, inject, signal } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatToolbarModule } from '@angular/material/toolbar'
 import { RouterOutlet } from '@angular/router'
 import { NotificationService } from './services'
 import { FilesUploadModule, SpinnerComponent } from './shared'
@@ -7,9 +11,43 @@ import { FilesUploadModule, SpinnerComponent } from './shared'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, SpinnerComponent, FilesUploadModule],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    SpinnerComponent,
+    FilesUploadModule,
+  ],
+  template: ` <mat-sidenav-container>
+    <mat-sidenav #menu><p>Elemento Sidenav</p></mat-sidenav>
+    <mat-sidenav-content>
+      <mat-toolbar color="primary">
+        <button mat-icon-button (click)="menu.toggle()">
+          <mat-icon>menu</mat-icon>
+        </button>
+        <span>Edificaciones Store</span>
+      </mat-toolbar>
+      <main>
+        <router-outlet></router-outlet>
+      </main>
+    </mat-sidenav-content>
+  </mat-sidenav-container>`,
+  styles: [
+    `
+      mat-sidenav-container,
+      mat-sidenav-content,
+      mat-sidenav,
+      mat-nav-list {
+        height: 100%;
+      }
+      mat-sidenav {
+        width: 250px;
+      }
+    `,
+  ],
 })
 export class AppComponent {
   private _notificationService: NotificationService;
